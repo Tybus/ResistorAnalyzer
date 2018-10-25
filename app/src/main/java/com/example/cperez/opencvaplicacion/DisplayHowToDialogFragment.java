fe99bccbd4052664dacf8d7f64ac10fb7e4eb1a5
cisaacpeq89 <cisaacpeq89@gmail.com>
2018-10-24 20:50:07 -0600

    Inico de programa incorporacion de libreria openCV

---
+++ app/src/main/java/com/example/cperez/opencvaplicacion/DisplayHowToDialogFragment.java
@@ -0,0 +1,29 @@
+package com.example.cperez.opencvaplicacion;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class DisplayHowToDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_howto)
+                .setNeutralButton(R.string.got_it, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+                /*
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+                */
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
