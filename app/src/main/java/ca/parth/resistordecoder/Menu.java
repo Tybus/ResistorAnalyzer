e2d36d63b0fde5919a4b86e03e80b0a7ac3c6340
cisaacpeq89 <cisaacpeq89@gmail.com>
2018-10-28 13:22:32 -0600

    Finalizacion de aplicacion a tiempo real

---
+++ app/src/main/java/ca/parth/resistordecoder/Menu.java
@@ -0,0 +1,43 @@
+package ca.parth.resistordecoder;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Menu extends AppCompatActivity {
+    private static final int PICK_IMAGE = 100;
+    final DisplayHowToDialogFragment HowToDialog = new DisplayHowToDialogFragment();
+    Uri imageURI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void displayHowTo(View view) {
+        HowToDialog.show(getSupportFragmentManager(), "How to use");
+    }
+
+    public void startCapture(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void browseImage(View view) {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageURI = data.getData();
+            //imageView.setImageUri(imageUri);
+        }
+    }
+}
